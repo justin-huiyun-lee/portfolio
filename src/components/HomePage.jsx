6e4d17c2f5192b92ee15ab19d74b17d9dd765344
Justin Lee <justinlee2364@gmail.com>
2023-06-13 04:46:32 -0700

    fixed weird text reponsivity in home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div id = "home" className="h-screen flex flex-col items-center justify-center bg-cover bg-center bg-background-image-forest border-b-8 border-black">
-      <h1 className="text-6xl -mt-10 font-bold text-white">HELLO</h1>
-      <h2 className="text-3xl text-white p-4">MY NAME IS</h2>
-      <h1 className="text-8xl font-bold text-white">JUSTIN LEE</h1>
+      <h1 className="md:text-6xl sm:text-5xl text-4xl -mt-10 font-bold text-white">HELLO</h1>
+      <h2 className="md:text-3xl sm:text-2xl text-xl text-white p-4">MY NAME IS</h2>
+      <h1 className="md:text-8xl sm:text-7xl text-6xl font-bold text-white">JUSTIN LEE</h1>
     </div>
   );
 };
